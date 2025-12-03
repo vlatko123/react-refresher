10b000ade47d64d4481c03d59e7e1170e8762d5e
vlatko123 <vlatkobogojevski@yahoo.com>
2025-12-04 00:42:44 +0100

    props, state, dynamic rendering of the lists

---
+++ src/components/TabButton/TabButton.jsx
@@ -0,0 +1,7 @@
+export const TabButton = ({children, onSelect, isSelected}) => {
+    return (
+        <li>
+            <button className={isSelected ? "active" : ''} onClick={onSelect}>{children}</button>
+        </li>
+    )
+}
